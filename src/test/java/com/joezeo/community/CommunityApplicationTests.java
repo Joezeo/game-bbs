02c618f6e7947b2248903347bc8eec942ed95ff0
Joezeo <1252795700@qq.com>
2020-01-28 22:43:34 +0800

    添加redis配置，创建RedisUtils工具类

--- src/test/java/com/joezeo/community/CommunityApplicationTests.java
+++ src/test/java/com/joezeo/community/CommunityApplicationTests.java
@@ -1,7 +1,10 @@
 package com.joezeo.community;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class CommunityApplicationTests {
