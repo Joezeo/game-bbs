6fa7f7029abf78a42d3456df8fab8571fadf199a
Joezeo <1252795700@qq.com>
2020-02-23 15:37:45 +0800

    添加Activiti5依赖

--- src/main/java/com/joezeo/community/CommunityApplication.java
+++ src/main/java/com/joezeo/community/CommunityApplication.java
@@ -1,11 +1,13 @@
 package com.joezeo.community;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+//集成activiti工作流 增加exclude = SecurityAutoConfiguration.class
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan("com.joezeo.community.mapper")
 @EnableScheduling
 public class CommunityApplication {
